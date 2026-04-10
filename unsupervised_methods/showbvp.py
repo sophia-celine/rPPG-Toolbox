f62957e31c95100332c4774bccb50a0d3696c28c
Sophia <sophiaceline@usp.br>
2026-04-10 14:34:25 -0300

    L7 preliminary results

--- unsupervised_methods/showbvp.py
+++ unsupervised_methods/showbvp.py
@@ -8,7 +8,7 @@ fs = 30
 
 
 # bvp_data = '/home/sophia/rPPG-Toolbox/BVPresults/BVP_GREEN_1_0.txt'
-bvp_data = '/home/sophia/rPPG-Toolbox/BVPresults/BVP_CHROM_0_0.txt'
+bvp_data = '/home/sophia/rPPG-Toolbox/BVPresults/BVP_POS_0_0.txt'
 
 sinal = np.loadtxt(bvp_data)
 metodo = Path(bvp_data).name.split("BVP_")[1].split("_0_0")[0]
